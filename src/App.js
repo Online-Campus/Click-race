d064ee063d9d8caa7c888725e33d452d65fcb8da
samagragupta <samagraguptajpr@gmail.com>
2020-09-04 18:26:11 +0530

    Created color game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ColorGame from './components/color_game/color_game';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ColorGame></ColorGame>
     </div>
   );
 }
---
+++ src/components/color_game/color_game.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './color_game.css';
+
+const colors = ["yellow", "green", "blue", "pink", "orange"];
+
+
+class ColorGame extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            start_time: new Date(),
+            time: 4000,
+            classes: "",
+            total_time: 0,
+            index: 0
+        };
+    }
+
+    start = () => {
+        if(this.state.index > 5){
+            alert(`Great your time is ${this.state.total_time}`);
+        }
+        
+        setTimeout(() => {
+            this.setState({
+                classes: colors[this.state.index],
+                start_time: new Date()
+            });
+        this.setState(prevstate => ({ index: prevstate.index + 1}));
+
+        console.log('total time',this.state.total_time);
+            
+        }, Math.floor(Math.random() * 3000) + 4000  )
+    }
+
+    game = () => {
+        let time = new Date() - this.state.start_time;
+        console.log('time', time);
+        this.setState(prevstate => ({ total_time: prevstate.total_time + time}));
+        this.setState({classes: ""});
+        this.start();
+    }
+
+
+    render() {
+
+      return (
+        <div>
+            <div className="heading">Click on the frame as soon as the color changes, as soon as possible</div>
+            <div onClick={this.game} className={`box ${this.state.classes}`}></div>
+            <button className="button" onClick={this.start}>
+                Start the game !!
+            </button>
+        </div>
+      );
+    }
+  }
+
+export default ColorGame;
